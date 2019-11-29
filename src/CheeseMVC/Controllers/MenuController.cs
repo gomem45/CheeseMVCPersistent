0d903ba5e8615a5f3bd2981e2731306b89470793
Miguel Gomez <mgomez.developer@gmail.com>
2019-11-29 18:25:05 -0500

    Create AddItem POST method to process form

--- src/CheeseMVC/Controllers/MenuController.cs
+++ src/CheeseMVC/Controllers/MenuController.cs
@@ -76,6 +76,37 @@ namespace CheeseMVC.Controllers
             return View(new AddMenuItemViewModel(menu, cheeses));
         }
 
+        [HttpPost]
+        public IActionResult AddItem(AddMenuItemViewModel addMenuItemViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var cheeseID = addMenuItemViewModel.CheeseID;
+                var menuID = addMenuItemViewModel.MenuID;
+
+                IList<CheeseMenu> existingItems = context.CheeseMenus.Where(
+                    cm => cm.CheeseID == cheeseID).Where(cm => cm.MenuID == menuID).ToList();
+
+                if (existingItems.Count == 0)
+                {
+                    CheeseMenu menuItem = new CheeseMenu
+                    {
+                        Cheese = context.Cheeses.Single(c => c.ID == cheeseID),
+                        Menu = context.Menus.Single(m => m.ID == menuID)
+                    };
+
+                    context.CheeseMenus.Add(menuItem);
+                    context.SaveChanges();
+                }
+
+                return Redirect(String.Format("/Menu/ViewMenu/{0}", addMenuItemViewModel.MenuID));
+            };
+
+            return View(addMenuItemViewModel);    
+        }
+
+
+
 
 
         
